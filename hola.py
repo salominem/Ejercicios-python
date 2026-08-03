7efb6ac848264b87ebbd65cc59a7aac65e9c3ccf
Salominem <sebasalomoncn@gmail.com>
2026-08-03 20:19:33 -0300

    probandoholamundo

--- hola.py
+++ hola.py
@@ -1,1 +1,27 @@
-print ("Hola Mundo")
+# primer Hola mundo
+# print ("Hola Mundo")
+
+# suma de dos enteros
+# num1 = 1
+# num2 = 2
+# print(num1+ num2)
+
+# esta mal
+# num1 = input("Ingrese el primer numero: ")
+# num2 = input("Ingrese el segundo numero: ")
+# print(num1+ num2)
+
+# esta bien
+# num1 = int(input("Ingrese el primer numero: "))
+# num2 = int(input("Ingrese el segundo numero: "))
+# print(num1+ num2)
+
+# grados = int(input("Cuantos grados hace? "))
+
+# if grados >= 30 and grados < 50:
+#     print("Me derrito")
+# elif grados <= 29 and grados >= 15:
+#     print("Esta lindo")
+# else:
+#     print("Hace frio")
+
