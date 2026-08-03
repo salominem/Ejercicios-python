85a805afe585c87be22d05ecbfc4276fa00f25a7
Salominem <sebasalomoncn@gmail.com>
2026-08-02 23:54:44 -0300

    CalculadoraSalop3

--- Python/calculadora.py
+++ Python/calculadora.py
@@ -1,4 +1,4 @@
-print("===============================")
+print("\n===============================")
 print("=    Calculadora Salominem    =")
 print("===============================")
 
@@ -15,21 +15,21 @@ opcion = input("\nElige una opcion: ")
 if opcion == "1":
     resultado = numero_1 + numero_2
     print(f"\n{numero_1} + {numero_2} = {resultado}")
-    print(f"El resultado de la Suma es: {resultado} ")
+    print(f"El resultado de la Suma es: {resultado}\n")
 elif opcion == "2":
     resultado = numero_1 - numero_2
     print(f"\n{numero_1} - {numero_2} = {resultado}")
-    print(f"El resultado de la Resta es: {resultado} ")
+    print(f"El resultado de la Resta es: {resultado}\n")
 elif opcion == "3":
     resultado = numero_1 * numero_2
-    print(f"\n{numero_1} * {numero_2} = {resultado}")
-    print(f"El resultado de la Multiplicacion es: {resultado} ")
+    print(f"\n{numero_1} * {numero_2} = {resultado}\n")
+    print(f"El resultado de la Multiplicacion es: {resultado}\n")
 elif opcion == "4":
     resultado = numero_1 / numero_2
     print(f"\n{numero_1} / {numero_2} = {resultado}")
-    print(f"El resultado de la Division es: {resultado} ")
-
-    
+    print(f"El resultado de la Division es: {resultado}\n")
+else:
+    print("Opcion Invalida.\n")
         
         
 
