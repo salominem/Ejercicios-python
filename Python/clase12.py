77a7cba825c2d4f23c1f0d8fe0b94b57ac671c4c
Salominem <sebasalomoncn@gmail.com>
2026-08-18 19:36:07 -0300

    Funciones nueva - Python

--- Python/clase12.py
+++ Python/clase12.py
@@ -4,3 +4,26 @@ def saludo():
     print("Python")
     
 saludo()
+# %%
+
+
+def recordatorio(nombre, hora):
+    print(f"Hola {nombre}, tu turno es el jueves a las {hora}")
+    
+recordatorio("Sebastian", "17")
+recordatorio("Sebastian", "17")
+recordatorio("Sebastian", "17")
+# %%
+
+def calculo_total(precio, cantidad):
+    print(f"El total es: {precio * cantidad}")
+    
+calculo_total(1500, 3)
+calculo_total(2500, 4)
+calculo_total(3500, 5)
+# %%
+def calculo_total(precio, cantidad):
+    return precio * cantidad
+
+total = calculo_total(1500, 3)
+print(total)
